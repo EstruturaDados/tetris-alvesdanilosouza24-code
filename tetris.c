3f524a5cc9660d3ae710a09f6b484ee0c8b66dcf
alvesdanilosouza24-code <alvesdanilosouza24@gmail.com>
2025-10-23 01:08:09 +0000

    estrutura do codigo (nivel novato)

--- tetris.c
+++ tetris.c
@@ -10,10 +10,10 @@ typedef struct {
 #define MAX 5
 
 typedef struct {
-    fila itens[MAX];
+    Peça itens[MAX];
     int inicio;
     int fim;
-    total;
+    int total;
 } fila;
 
 void inicializarfila(fila *f) { 
@@ -26,7 +26,7 @@ int filacheia(fila *f) {
     return f->total == MAX;
 }
 
-int filacheia(fila *f) {
+int filavazia(fila *f) {
     return f->total == 0;
 }
 
@@ -53,8 +53,41 @@ void remover(fila *f, Peça *p) {
     f->total--;
 }
 
+void mostrarfila(fila *f) {
+    printf("fila: ");
+
+    for (int i = 0, idx = f->inicio; i < f->total; i++, idx = (idx + 1) % MAX) {
+        printf("[%s, %d]", f->itens[idx].nome, f->itens[idx].id);
+    }
+
+    printf("\n");
+}
 
 int main() {
+    fila f;
+    inicializarfila(&f); // incializa a fila
 
+   
+    Peça p1 = {"T", 0};
+    Peça p2 = {"O", 1};
+    Peça p3 = {"L", 2};
+    Peça p4 = {"I", 3};
+    Peça p5 = {"I", 4};
+
+    // inserir algumas peças na  fila
+    inserir(&f, p1);
+    inserir(&f, p2);
+    inserir (&f, p3);
+    inserir(&f, p4);
+    inserir(&f, p5);
+
+    mostrarfila(&f); // mostrar fila antes da remoção
+
+ // Remover peça da fila
+ Peça removida;
+ remover(&f, &removida); // aqui usamos a função de removeção
+ printf("peça removida: %s, %d\n", removida.nome, removida.id);
+
+ return 0;
 }
 
